61d14e3f7ec54ee89e452a1b37a5658c2dc6af15
Ivan Tiunov <tiunov.i@gubkin.ru>
2019-03-05 02:15:04 +0300

    Programming assignment.

--- Course_3_Getting_and_cleaning_data/assignment/import_data.R
+++ Course_3_Getting_and_cleaning_data/assignment/import_data.R
@@ -12,11 +12,37 @@ path_to_xtrain      <- file.path("dataset", "train", "X_train.txt")
 path_to_ytrain      <- file.path("dataset", "train", "y_train.txt")
 path_to_subj_train  <- file.path("dataset", "train", "subject_train.txt")
 
-# Reading files
+# Finding out column indicies that we want to import
 features    <- fread(path_to_features)
-x_test      <- fread(path_to_xtest)
-y_test      <- fread(path_to_ytest)
-subj_test   <- fread(path_to_subj_test)
-x_train     <- fread(path_to_xtrain)
-y_train     <- fread(path_to_ytrain)
-subj_train  <- fread(path_to_subj_train)
+foi <- which(grepl("mean\\(\\)|std\\(\\)|meanfreq\\(\\)", tolower(features$V2)))   # features of interest
+
+# Reading files
+y_test      <- fread(path_to_ytest) %>% as_tibble()
+subj_test   <- fread(path_to_subj_test) %>% as_tibble()
+x_test      <- fread(path_to_xtest, select = foi) %>% as_tibble() %>%
+               setNames(features$V2[foi]) %>% mutate(act = y_test$V1, sub = subj_test$V1, set = "test")
+
+y_train     <- fread(path_to_ytrain) %>% as_tibble()
+subj_train  <- fread(path_to_subj_train) %>% as_tibble()
+x_train     <- fread(path_to_xtrain, select = foi) %>% as_tibble() %>%
+               setNames(features$V2[foi]) %>% mutate(act = y_train$V1, sub = subj_train$V1, set = "train")
+
+data <- bind_rows(x_test, x_train)
+
+
+# Column names contain variables, lets try and fix this
+
+# Gather columns that shoud be variables
+test_data <- gather(data, all_vars, val, -act, -sub, -set)
+
+# Now let's try and separate them in the following variables:
+# domain: time or frequency
+# dir: X, Y or Z
+# sensor: Acc or Gyro
+# acceleration: Body or Gravity
+# dontknow: mean or std
+
+# In order to separate variables we need to insert special symbols in the names.
+# But we should do it after reading features (so that we operate on fewer number of columns) 
+test_data$all_vars <- gsub("Body", "=Body=", test_data$all_vars)
+test_data$all_vars <- gsub("Gravity", "=Gravity=", test_data$all_vars)
